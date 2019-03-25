2e3751e9978fabcd2109ed6c9a1f0c7705ea1301
yymtheGreat <txbmdysyym@163.com>
2019-03-25 20:01:10 +0800

    登录页面设计完成

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,32 +1,56 @@
 <template>
-    <div class="ass">
-      <div class="box">
+  <div class="ass">
+    <div class="box">
+      <el-form label-position="top" label-width="80px" :model="LoginForm">
+        <el-form-item label="用户名">
+          <el-input v-model="LoginForm.name"></el-input>
+        </el-form-item>
+        <el-form-item label="密码">
+          <el-input v-model="LoginForm.password"></el-input>
+        </el-form-item>
+        <el-form-item >
+          <el-button type="primary" @click.prevent='onSubmit' class="theButton">登录</el-button>
+        </el-form-item>
+      </el-form>
 
-      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Login',
-  data () {
-    return {}
-  }
+  name: "Login",
+  data() {
+      return {
+        labelPosition: 'right',
+        LoginForm: {
+          name: '',
+          password: '',
 
-}
+        }
+      };
+    },
+  methods:{
+    onSumit () {}
+  }
+};
 </script>
 
 <style scoped>
-    .ass {
-      height: 100%;
-      background-color: rgb(110, 110, 110) !important;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-    }
-    .box {
-      height: 400px;
-      width: 500px;
-      background-color: white;
-    }
+.ass {
+  height: 100%;
+  background-color: rgb(110, 110, 110) !important;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.box {
+  border-radius: 20px;
+  width: 500px;
+  background-color: white;
+  padding: 50px;
+}
+.theButton {
+  width: 100%;
+}
 </style>
