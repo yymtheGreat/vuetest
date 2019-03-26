27c2ff9e429eba2f9ce2ed98fadb482be2cc7bf0
yymtheGreat <txbmdysyym@163.com>
2019-03-26 16:19:28 +0800

    表单验证完成

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,39 +1,78 @@
 <template>
   <div class="ass">
     <div class="box">
-      <el-form label-position="top" label-width="80px" :model="LoginForm">
-        <el-form-item label="用户名">
-          <el-input v-model="LoginForm.name"></el-input>
+      <el-form label-position="top" label-width="80px" :model="LoginForm" :rules="loginFormRules" ref='loginFormEl'>
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="LoginForm.username"></el-input>
         </el-form-item>
-        <el-form-item label="密码">
+        <el-form-item label="密码" prop="password">
           <el-input v-model="LoginForm.password"></el-input>
         </el-form-item>
-        <el-form-item >
-          <el-button type="primary" @click.prevent='onSubmit' class="theButton">登录</el-button>
+        <el-form-item>
+          <el-button type="primary" @click.prevent="onSubmit" class="theButton">登录</el-button>
         </el-form-item>
       </el-form>
-
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import { async } from 'q';
 export default {
-  name: "Login",
-  data() {
-      return {
-        labelPosition: 'right',
-        LoginForm: {
-          name: '',
-          password: '',
-
-        }
-      };
+  name: 'Login',
+  data () {
+    return {
+      labelPosition: 'right',
+      LoginForm: {
+        username: '',
+        password: ''
+      },
+      loginFormRules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 8, message: '长度在 3 到 8 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 3, max: 15, message: '长度在 3 到 15 个字符', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    async onSubmit () {
+      this.$refs.loginFormEl.validate((valid) => {
+          if (valid) {
+            this.login()
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        })
     },
-  methods:{
-    onSumit () {}
+    async login () {
+               const resData = await axios.post(
+        'http://localhost:8888/api/private/v1/login',
+        this.LoginForm
+      )
+      console.log(resData)
+      if (resData.data.meta.status === 200) {
+        this.$message({
+          message: '登录成功',
+          type: 'success'
+        })
+      } else {
+        this.$message({
+          message: `${resData.data.meta.msg}`,
+          type: 'warning'
+        })
+      }
+        }
+
+
   }
-};
+}
 </script>
 
 <style scoped>
